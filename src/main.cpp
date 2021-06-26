cb600949dcfd6a9549601df0abeda4a2854e0553
ethanhuang03 <ethanhuang301@gmail.com>
2021-06-26 19:51:11 +1200

    created 'template' code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,46 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+okapi::Controller controller;
+
+okapi::Motor frontRightDrive(1);
+okapi::Motor backRightDrive(-2);
+okapi::Motor frontLeftDrive(3);
+okapi::Motor backLeftDrive(-4);
+
+okapi::MotorGroup RightDrive({frontRightDrive, backRightDrive});
+okapi::MotorGroup LeftDrive({frontLeftDrive, backLeftDrive});
+
+okapi::IntegratedEncoder rightEncoder(backRightDrive);
+okapi::IntegratedEncoder leftEncoder(backLeftDrive);
+
+//Green gearset, 4 in wheel diam, 11.5 in wheel track
+std::shared_ptr<ChassisController> drive = ChassisControllerBuilder().withMotors(LeftDrive, RightDrive).withDimensions(AbstractMotor::gearset::green, {{4_in, 11.5_in}, imev5GreenTPR}).withSensors(leftEncoder, rightEncoder).build();
+
+okapi::ControllerButton R1(ControllerDigital::R1);
+okapi::ControllerButton R2(ControllerDigital::R2);
+okapi::ControllerButton L1(ControllerDigital::L1);
+okapi::ControllerButton L2(ControllerDigital::L2);
+
+okapi::ControllerButton X(ControllerDigital::X);
+okapi::ControllerButton Y(ControllerDigital::Y);
+okapi::ControllerButton A(ControllerDigital::A);
+okapi::ControllerButton B(ControllerDigital::B);
+
+okapi::ControllerButton Up(ControllerDigital::up);
+okapi::ControllerButton Down(ControllerDigital::down);
+okapi::ControllerButton Left(ControllerDigital::left);
+okapi::ControllerButton Right(ControllerDigital::right);
+
+
+
+void tank_drive(){
+	drive->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::rightY));
+}
+
+void arcade_drive(){
+	drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::leftX));
+}
+
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -16,77 +51,30 @@ void on_center_button() {
 	}
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
+	/*Logger::setDefaultLogger( //log output to pros terminal
+		std::make_shared<Logger>(
+			TimeUtilFactory::createDefault().getTimer(),
+			"/ser/sout",
+			Logger::LogLevel::debug
+		)
+	);*/
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::set_text(1, "King's B | 2923B");
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+void autonomous() {
+	drive->moveDistance(12_in);
+	drive->turnAngle(90_deg);
+}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	tank_drive();
+	//arcade_drive();
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
-	}
+	pros::delay(10);
 }
