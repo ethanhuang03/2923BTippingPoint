dfabf12828fdae2766b34ca715e3563067210da8
ethanhuang03 <ethanhuang301@gmail.com>
2021-07-25 20:02:13 +1200

    functions glore

--- src/main.cpp
+++ src/main.cpp
@@ -48,9 +48,14 @@ void initialize() {
 	drive = ChassisControllerBuilder()
 		.withMotors(LeftDrive, RightDrive)
 		//Green gearset, 4 in wheel diam, 11.5 in wheel track
-		.withDimensions(AbstractMotor::gearset::green, {{4_in, 11.5_in}, imev5GreenTPR})
+		.withDimensions(AbstractMotor::gearset::green, {{4_in, 15_in}, imev5GreenTPR})
 		.withSensors(leftEncoder, rightEncoder)
-		.withOdometry()
+		.withOdometry({{4_in, 15_in}, quadEncoderTPR}) // or .withOdometry()
+		.withGains(
+			{0, 0, 0}, // Distance controller gains
+			{0, 0, 0}, // Turn controller gains
+			{0, 0, 0}  // Angle controller gains (helps drive straight)
+		)
 		.buildOdometry();
 /*
 	drive = ChassisControllerBuilder()
@@ -79,34 +84,260 @@ void tank_drive(){
 }
 
 void arcade_drive(){
+	pros::lcd::set_text(1, "Arcade Drive");
 	drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::leftX));
+}
+
+void red_front(){
+	drive->moveDistance(12_in);
+	drive->turnAngle(90_deg);
+}
+
+void red_back(){
+
+}
+
+void do_nothing(){
+
+}
+
+void blue_front(){
+
+}
+
+void blue_back(){
+
+}
+
+void skills(){
+
 }
 
 void autonomous() {
 	if(selector::auton == 1){ //Red Front
-		drive->moveDistance(12_in);
-		drive->turnAngle(90_deg);
+		red_front();
 	}
 	else if(selector::auton == 2){ //Red Back
+		red_back();
 	}
 	else if(selector::auton == 3){ //Do Nothing
+		do_nothing();
 	}
 	else if(selector::auton == -1){ //Blue Front
+		blue_front();
 	}
 	else if(selector::auton == -2){ //Blue Back
+		blue_back();
 	}
 	else if(selector::auton == -3){ //Do Nothing
+		do_nothing();
 	}
 	else if(selector::auton == 0){ //Skills
+		skills();
+	}
+}
+
+void R1(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void R2(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void L1(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void L2(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void X(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void Y(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void A(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void B(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void Up(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void Down(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void Left(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void Right(bool run){
+	if(run){
+
+	}
+	else{
+
+	}
+}
+
+void button_macros(){
+	if(button_R1.isPressed()){
+		R1(true);
+	}
+	else{
+		R1(false);
+	}
+
+	if(button_R2.isPressed()){
+		R2(true);
+	}
+	else{
+		R2(false);
+	}
+
+	if(button_L1.isPressed()){
+		L1(true);
+	}
+	else{
+		L1(false);
+	}
+
+	if(button_L2.isPressed()){
+		L2(true);
+	}
+	else{
+		L2(false);
+	}
+
+	if(button_X.isPressed()){
+		X(true);
+	}
+	else{
+		X(false);
+	}
+
+	if(button_Y.isPressed()){
+		Y(true);
+	}
+	else{
+		Y(false);
+	}
+
+	if(button_A.isPressed()){
+		A(true);
+	}
+	else{
+		A(false);
+	}
+
+	if(button_B.isPressed()){
+		B(true);
+	}
+	else{
+		B(false);
+	}
+
+	if(button_Up.isPressed()){
+		Up(true);
+	}
+	else{
+		Up(false);
+	}
+
+	if(button_Down.isPressed()){
+		Down(true);
+	}
+	else{
+		Down(false)
+	}
+
+	if(button_Left.isPressed()){
+		Left(true);
+	}
+	else{
+		Left(false);
+	}
+
+	if(button_Right.isPressed()){
+		Right(true);
+	}
+	else{
+		Right(false);
 	}
 }
 
 void opcontrol() {
+	pros::lcd::set_text(2, "User Control");
 	while(true){
-		pros::lcd::set_text(2, "Tankasdf Drive");
-		tank_drive();
-		//arcade_drive();
+		tank_drive(); //arcade_drive();
+		button_macros();
 
 		pros::delay(10);
 	}
+
 }
