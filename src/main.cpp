e6631819c07e0e52715a77b22ac80594ecfda275
ethanhuang03 <ethanhuang301@gmail.com>
2022-03-11 11:14:02 +1300

    asdf

--- src/main.cpp
+++ src/main.cpp
@@ -8,41 +8,14 @@ std::shared_ptr<ChassisController> drive;
 
 auto backRightDrive = Motor(-1);
 auto frontRightDrive = Motor(-2);
-auto backLeftDrive = Motor(3);
-auto frontLeftDrive = Motor(4);
+auto topRightDrive = Motor(3);
 
-auto lift = Motor(5);
-auto intake = Motor(6);
-auto mogo = Motor(7);
-auto clamp = Motor(8);
+auto backLeftDrive = Motor(4);
+auto frontLeftDrive = Motor(5);
+auto topLeftDrive = Motor(-6);
 
-auto RightDrive = MotorGroup({frontRightDrive, backRightDrive});
-auto LeftDrive = MotorGroup({frontLeftDrive, backLeftDrive});
-
-bool spin_flag = false;
-int spin_velocity = 100;
-int spin_v = 100;
-
-void move(float ft){
-
-}
-
-/*
-void turn(int degrees, bool right){
-	if(right){
-		backRightDrive.moveAbsolute(-degrees);
-		frontRightDrive.moveAbsolute(-degrees)
-		backLeftDrive.moveAbsolute(degrees);
-		backLeftDrive.moveAbsolute(degrees);
-	}
-	else{
-		backRightDrive.moveAbsolute(degrees);
-		frontRightDrive.moveAbsolute(degrees)
-		backLeftDrive.moveAbsolute(-degrees);
-		backLeftDrive.moveAbsolute(-degrees);
-	}
-}
-*/
+auto RightDrive = MotorGroup({frontRightDrive, backRightDrive, topRightDrive});
+auto LeftDrive = MotorGroup({frontLeftDrive, backLeftDrive, topLeftDrive});
 
 void initialize() {
 	pros::lcd::initialize();
@@ -51,7 +24,7 @@ void initialize() {
 	drive = ChassisControllerBuilder()
 		.withMotors(LeftDrive, RightDrive)
 		//Green gearset, 4 in wheel diam, 11.5 in wheel track
-		.withDimensions({AbstractMotor::gearset::green, (36.0 / 60.0)}, {{3.25_in, 13.7795_in}, imev5GreenTPR})
+		.withDimensions({AbstractMotor::gearset::blue, (60.0 / 36.0)}, {{3.25_in, 13.7795_in}, imev5BlueTPR})
 		.withOdometry() // or .withOdometry({{4_in, 15_in}, quadEncoderTPR})
 		.withGains(
 			{0.002, 0, 0.000197}, // Distance controller gains
@@ -59,28 +32,7 @@ void initialize() {
 			{0.002, 0, 0.0001}  // Angle controller gains (helps drive straight)
 		)
 		.buildOdometry();
-		lift.setBrakeMode(AbstractMotor::brakeMode::hold);
 }
-
-std::shared_ptr<AsyncPositionController<double, double>> mogoController =
-  AsyncPosControllerBuilder()
-    .withMotor(7)
-    .build();
-
-std::shared_ptr<AsyncPositionController<double, double>> liftController =
-  AsyncPosControllerBuilder()
-    .withMotor(5)
-    .build();
-
-std::shared_ptr<AsyncPositionController<double, double>> intakeController =
-  AsyncPosControllerBuilder()
-    .withMotor(6)
-    .build();
-
-std::shared_ptr<AsyncPositionController<double, double>> clampController =
-  AsyncPosControllerBuilder()
-    .withMotor(8)
-    .build();
 
 void disabled() {}
 
@@ -88,57 +40,13 @@ void competition_initialize() {}
 
 void tank_drive(){
 	pros::lcd::set_text(1, "Tank Drive");
-	drive->getModel()->tank(master.getAnalog(ControllerAnalog::rightY), master.getAnalog(ControllerAnalog::leftY));
+	drive->getModel()->tank(master.getAnalog(ControllerAnalog::leftY), master.getAnalog(ControllerAnalog::rightY));
 }
 
 void red_front(){
-	int old = mogoController->getTarget();
-	mogoController->setTarget(1300);
-	liftController->setTarget(500);
-	mogoController->waitUntilSettled();
-
-	drive->moveDistanceAsync(-2_ft);
-	drive->waitUntilSettled();
-
-	mogoController->setTarget(old);
-	mogoController->waitUntilSettled();
-
-	intakeController->setMaxVelocity(100);
-	intakeController->setTarget(-1000000);
-
-	drive->setMaxVelocity(50);
-	drive->moveDistanceAsync(2.8_ft);
-/*
-	drive->turnAngle(-95_deg);
-	drive->moveDistanceAsync(1_ft);
-	drive->waitUntilSettled();
-	drive->turnAngle(-95_deg);
-	drive->setMaxVelocity(50);
-	drive->moveDistanceAsync(2.5_ft);
-	drive->waitUntilSettled();
-
-	drive->moveDistanceAsync(-5_ft);
-	drive->waitUntilSettled();
-*/
 }
 
 void red_back(){
-	int old = mogoController->getTarget();
-	mogoController->setTarget(1300);
-	liftController->setTarget(500);
-	mogoController->waitUntilSettled();
-
-	drive->moveDistanceAsync(-2_ft);
-	drive->waitUntilSettled();
-
-	mogoController->setTarget(old);
-	mogoController->waitUntilSettled();
-
-	intakeController->setMaxVelocity(100);
-	intakeController->setTarget(-1000000);
-
-	drive->setMaxVelocity(50);
-	drive->moveDistanceAsync(2.8_ft);
 }
 
 void do_nothing(){
@@ -185,45 +93,6 @@ void opcontrol() {
 	pros::lcd::set_text(2, "User Control");
 	while(true){
 		tank_drive();
-
-		if(partner.getDigital(ControllerDigital::R1) || master.getDigital(ControllerDigital::R1)){
-			spin_velocity = -200;
-			intake.moveVelocity(spin_velocity);
-		}
-		else if(partner.getDigital(ControllerDigital::R2) || master.getDigital(ControllerDigital::R2)){
-			spin_velocity = 200;
-			intake.moveVelocity(spin_velocity);
-		}
-
-		if(partner.getDigital(ControllerDigital::L1) || master.getDigital(ControllerDigital::L1)){
-			mogo.moveVelocity(-200);
-		}
-		else if(partner.getDigital(ControllerDigital::L2) || master.getDigital(ControllerDigital::L2)){
-			mogo.moveVelocity(200);
-		}
-		else{
-			mogo.moveVelocity(0);
-		}
-
-		if(partner.getDigital(ControllerDigital::X) || master.getDigital(ControllerDigital::X)){
-			clamp.moveVelocity(-100);
-		}
-		else if(partner.getDigital(ControllerDigital::Y) || master.getDigital(ControllerDigital::Y)){
-			clamp.moveVelocity(100);
-		}
-		else{
-			clamp.moveVelocity(0);
-		}
-
-		if(partner.getDigital(ControllerDigital::up) || master.getDigital(ControllerDigital::up)){
-			lift.moveVelocity(100);
-		}
-		else if(partner.getDigital(ControllerDigital::down) || master.getDigital(ControllerDigital::down)){
-			lift.moveVelocity(-100);
-		}
-		else{
-			lift.moveVelocity(0);
-		}
 
 		pros::delay(10);
 	}
