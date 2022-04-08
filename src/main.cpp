a6292962ee9f39d7fa3492c162fbc07edcb5d9c3
ethanhuang03 <ethanhuang301@gmail.com>
2022-04-08 17:57:10 +1200

    asdf

--- src/main.cpp
+++ src/main.cpp
@@ -72,13 +72,13 @@ void initialize() {
 
 		std::shared_ptr<AsyncMotionProfileController> driveController =
 		  AsyncMotionProfileControllerBuilder()
-				/*
+			/*
 		    .withLimits({
 		      1.0, // Maximum linear velocity of the Chassis in m/s
 		      2.0, // Maximum linear acceleration of the Chassis in m/s/s
 		      10.0 // Maximum linear jerk of the Chassis in m/s/s/s
 		    })
-				*/
+			*/
 		    .withOutput(drive)
 		    .buildMotionProfileController();
 }
